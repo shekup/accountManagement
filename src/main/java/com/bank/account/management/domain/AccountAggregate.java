38974d269a4e7d15c87cc0e01c770aa9ad8cc028
Abhishek Upadhyay <shek.up@gmail.com>
2020-09-05 07:20:14 +0530

    Update AccountAggregate.java

--- src/main/java/com/bank/account/management/domain/AccountAggregate.java
+++ src/main/java/com/bank/account/management/domain/AccountAggregate.java
@@ -21,8 +21,20 @@ public class AccountAggregate {
 		AggregateLifecycle.apply(new CreateAccountEvent(accountId, createAccountCommand.getAccountName(), createAccountCommand.getProductClass()));
 	}
 	
+	// The constructor will not be no args instead relevant values should be passed to CreditMoneyCommand
+	// account Id will not be generated while this command instead, account Id will come as input parameter
+	@CommandHandler
+	public void CreditAccountHandler(CreditMoneyCommand creditMoneyCommand) {
+		AggregateLifecycle.apply(new AccountBalanceUpdatedEvent());
+	}
+	
 	@EventSourcingHandler
 	public void on(CreateAccountEvent createAccountEvent) {
+		
+	}
+	
+	@EventSourcingHandler
+	public void on(AccountBalanceUpdatedEvent accountBalanceUpdatedEvent) {
 		
 	}
 
